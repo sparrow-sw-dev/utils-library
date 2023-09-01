8d361d37f2a8159ad47b3d7a9d92f66864f7af0d
Aaron Kauffman <aaron.christian.kauffman@gmail.com>
2023-08-31 20:12:32 -0700

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+import log from './src/log.js';
+import error from './src/error.js';
+import { isValidEmail, isLocalDevelopment, clone, sleep } from './src/helpers.js';
+
+export { log, error, isValidEmail, isLocalDevelopment, clone, sleep };
